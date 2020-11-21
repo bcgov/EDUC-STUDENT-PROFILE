365e641ef4ac3dcd521c4fe48d9214e062e797dc
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2020-11-20 19:49:34 -0800

    changed the red lock key...

--- backend/src/schedulers/student-profile-saga-check-scheduler.js
+++ backend/src/schedulers/student-profile-saga-check-scheduler.js
@@ -64,7 +64,7 @@ try {
     log.info('starting findAndRemoveStaleSagaRecord');
     const redLock = redisUtil.getRedLock();
     try {
-      await redLock.lock('locks:remove-stale-saga-record', 6000); // no need to release the lock as it will auto expire after 6000 ms.
+      await redLock.lock('locks:remove-stale-saga-record-student-profile', 6000); // no need to release the lock as it will auto expire after 6000 ms.
       const staleSagas = findStaleSagaRecords(await redisUtil.getProfileRequestSagaEvents());
       log.info(`found ${staleSagas.length} stale GMP or UMP saga records`);
 
@@ -72,7 +72,7 @@ try {
         await removeStaleSagas(staleSagas);
       }
     } catch (e) {
-      log.info(`locks:remove-stale-saga-record, check other pods. ${e}`);
+      log.info(`locks:remove-stale-saga-record-student-profile, check other pods. ${e}`);
     }
   });
   removeStaleSagaRecordFromRedis.start();
