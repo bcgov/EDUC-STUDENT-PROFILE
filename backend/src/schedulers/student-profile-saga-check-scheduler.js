f7febced4b4d944822e2d7241402404b1c88454c
OMPRAKASH MISHRA <32200996+mishraomp@users.noreply.github.com>
2021-01-27 14:30:05 -0800

    Update student-profile-saga-check-scheduler.js

--- backend/src/schedulers/student-profile-saga-check-scheduler.js
+++ backend/src/schedulers/student-profile-saga-check-scheduler.js
@@ -33,7 +33,7 @@ function findStaleSagaRecords(inProgressSagas) {
 async function removeStaleSagas(staleSagas) {
   let sagaRecordFromAPIPromises = [];
   try {
-    const data = await getApiCredentials(); // get the tokens first to make api calls.
+    const data = await getApiCredentials(config.get('oidc:clientId'), config.get('oidc:clientSecret')); // get the tokens first to make api calls.
     for (const saga of staleSagas) {
       sagaRecordFromAPIPromises.push(getData(data.accessToken, `${config.get('server:profileSagaAPIURL')}/${saga.sagaId}`));
     }
