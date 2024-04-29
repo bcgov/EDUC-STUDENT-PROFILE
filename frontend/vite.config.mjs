f3d49d16e72148f584cc7f878d8b0b91f9c23f00
Trevor Richards <trev@trevdev.ca>
2024-04-29 11:59:03 -0700

    fix: dependency on tranformAssetUrls
    This isn't a necessary tranformation.  Neither is using the vuetify vite
    plugin

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,13 +1,9 @@
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 
 export default defineConfig({
   plugins: [
-    vue({
-      template: { transformAssetUrls }
-    }),
-    vuetify()
+    vue()
   ],
   server: {
     port: 8081,
