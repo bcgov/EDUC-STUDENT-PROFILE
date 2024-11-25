99c7678645de6e761a7b8addf3659432b3460e0d
Trevor Richards <trev@trevdev.ca>
2024-11-25 13:46:24 -0800

    feat: prepare base router for new keycloak realm

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -55,6 +55,7 @@ router.get('/error', (_req, res) => {
 
 function addBaseRouterGet(strategyName, callbackURI) {
   router.get(callbackURI, passport.authenticate(strategyName, {
+    scope: ['openid', 'profile'],
     failureRedirect: 'error'
   }));
 }
